4befb310d5146003eb7bcedda93506489cd0a8e3
rafajperez <rafael_cordas@hotmail.com>
2024-06-21 15:37:29 -0300

    install of all features

--- my_portfolio/src/main.tsx
+++ my_portfolio/src/main.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+
+
+const router = createBrowserRouter ([
+  {
+    path: "/",
+    element: <div>Rafae Perez Portfolio</div>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
